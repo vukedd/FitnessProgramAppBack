cfa9e91296adc2c6805a78ff49ab2841ad82e691
vukedd <vukedd@gmail.com>
2025-02-22 19:53:15 +0100

    Added initial spring boot project

---
+++ src/main/java/com/app/fitness/fitnesprogramapp/FitnesProgramAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.fitness.fitnesprogramapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesProgramAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnesProgramAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/fitness/fitnesprogramapp/FitnesProgramAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fitness.fitnesprogramapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesProgramAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
