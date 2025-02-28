2ba5aa1cf749e9fdc6a58a0cc36715cbc87a9bdb
vukedd <vukedd@gmail.com>
2025-02-28 18:21:17 +0100

    Added account verification through email

--- src/main/java/com/app/fitness/fitnesprogramapp/repositories/user/UserRepository.java
+++ src/main/java/com/app/fitness/fitnesprogramapp/repositories/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.app.fitness.fitnesprogramapp.repositories.user;
 
 import com.app.fitness.fitnesprogramapp.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationToken(String token);
+
+    @Query("SELECT u FROM User u WHERE u.username = ?1 AND u.enabled = true")
+    Optional<User> findVerifiedUserByUsername(String username);
 }
--- src/main/java/com/app/fitness/fitnesprogramapp/services/register/RegisterService.java
+++ src/main/java/com/app/fitness/fitnesprogramapp/services/register/RegisterService.java
@@ -2,18 +2,24 @@ package com.app.fitness.fitnesprogramapp.services.register;
 
 import com.app.fitness.fitnesprogramapp.dtos.register.UserRegisterRequestDto;
 import com.app.fitness.fitnesprogramapp.dtos.register.UserRegisterResponseDto;
+import com.app.fitness.fitnesprogramapp.dtos.verification.UserVerificationResponseDto;
 import com.app.fitness.fitnesprogramapp.exceptions.customExceptions.RegisterException;
+import com.app.fitness.fitnesprogramapp.exceptions.customExceptions.VerificationException;
 import com.app.fitness.fitnesprogramapp.models.user.User;
 import com.app.fitness.fitnesprogramapp.repositories.user.UserRepository;
+import com.app.fitness.fitnesprogramapp.services.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class RegisterService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
     public UserRegisterResponseDto sendUserRegisterRequest(UserRegisterRequestDto registerRequest) {
         if (userRepository.findByEmail(registerRequest.getEmail()).isPresent()) {
@@ -25,10 +31,30 @@ public class RegisterService {
         }
 
         User user = User.builder().username(registerRequest.getUsername()).email(registerRequest.getEmail()).password(passwordEncoder.encode(registerRequest.getPassword()))
-                .firstName(registerRequest.getFirstName()).lastName(registerRequest.getLastName()).build();
+                .firstName(registerRequest.getFirstName()).lastName(registerRequest.getLastName()).enabled(false).build();
+
+
+        String token = UUID.randomUUID().toString();
+        user.setVerificationToken(token);
 
         user = userRepository.save(user);
+        String confirmationUrl = "http://localhost:8080/api/auth/verify-email?token=" + token;
+        emailService.sendEmail(user.getEmail(), "Email Verification", "Click the following link to verify your email: \n" + confirmationUrl);
 
         return new UserRegisterResponseDto(user.getId(), "User successfully registered!", user.getUsername(), user.getEmail(), user.getPassword());
+    }
+
+    public UserVerificationResponseDto validateVerificationToken(String token) {
+
+        User user = userRepository.findByVerificationToken(token)
+                .orElseThrow(() -> new VerificationException("Token not Found!", VerificationException.ErrorType.TOKEN_NOT_FOUND));
+
+        if (user.isEnabled()) {
+            throw new VerificationException("Account is already verified!", VerificationException.ErrorType.ALREADY_VERIFIED);
+        }
+        user.setEnabled(true);
+        userRepository.save(user);
+
+        return new UserVerificationResponseDto("Your account is now verified!");
     }
 }
