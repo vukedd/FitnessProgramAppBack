afa02f59e3561873a922f705b0fdd8e5e5b5323e
vukedd <vukedd@gmail.com>
2025-06-06 00:35:06 +0200

    Modified account verification request handling

--- src/main/java/com/app/fitness/fitnesprogramapp/controllers/register/RegisterController.java
+++ src/main/java/com/app/fitness/fitnesprogramapp/controllers/register/RegisterController.java
@@ -3,12 +3,14 @@ package com.app.fitness.fitnesprogramapp.controllers.register;
 import com.app.fitness.fitnesprogramapp.dtos.register.UserRegisterRequestDto;
 import com.app.fitness.fitnesprogramapp.dtos.register.UserRegisterResponseDto;
 import com.app.fitness.fitnesprogramapp.dtos.verification.UserVerificationResponseDto;
+import com.app.fitness.fitnesprogramapp.exceptions.customExceptions.VerificationException;
 import com.app.fitness.fitnesprogramapp.services.register.RegisterService;
 import com.app.fitness.fitnesprogramapp.services.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.webauthn.api.UserVerificationRequirement;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @CrossOrigin
@@ -24,7 +26,19 @@ public class RegisterController {
     }
 
     @GetMapping("/verify-email")
-    public ResponseEntity<UserVerificationResponseDto> verifyEmail(@RequestParam("token") String token) {
-        return ResponseEntity.ok(userRegistrationService.validateVerificationToken(token));
+    public RedirectView verifyEmail(@RequestParam("token") String token) {
+        int res = 200;
+
+        try { userRegistrationService.validateVerificationToken(token);}
+        catch (VerificationException e) {
+            VerificationException.ErrorType exception = e.getErrorType();
+            res = switch (exception.ordinal()) {
+                case 0 -> 400;
+                case 1 -> 409;
+                default -> res;
+            };
+        }
+
+        return new RedirectView("http://repzly.netlify.app/?status=" + String.valueOf(res));
     }
 }
